a038fcaf2a1ae63da88857ac1744f2e206c9277c
Isaac A. Ferreira <isaacaufe@gmail.com>
2024-08-31 19:21:43 -0300

    ajusts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import SideBar from '@/components/Sidebar/SideBar.vue'
-import CardComponent from '@/components/CardComponent.vue'
+import CardComponent from '@/components/Card/CardComponent.vue'
 </script>
 
 <template>
