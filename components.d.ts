fb3b389c3d809f77fc479df1e26c4f6ec586bbc0
Isaac A. Ferreira <isaacaufe@gmail.com>
2024-09-30 21:11:03 -0300

    dist

--- components.d.ts
+++ components.d.ts
@@ -13,9 +13,7 @@ declare module 'vue' {
     CardComponent: typeof import('./src/components/Card/CardComponent.vue')['default']
     Checkbox: typeof import('primevue/checkbox')['default']
     Menu: typeof import('primevue/menu')['default']
-    OverlayBadge: typeof import('primevue/overlaybadge')['default']
     PrePraticeComponent: typeof import('./src/components/Card/PrePraticeComponent.vue')['default']
-    ProgressBar: typeof import('primevue/progressbar')['default']
     QuizComponent: typeof import('./src/components/Card/QuizComponent.vue')['default']
     QuizPrePraticeComponent: typeof import('./src/components/Card/QuizPrePraticeComponent.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
@@ -23,12 +21,5 @@ declare module 'vue' {
     SideBar: typeof import('./src/components/Sidebar/SideBar.vue')['default']
     SidebarHeader: typeof import('./src/components/Sidebar/SidebarHeader.vue')['default']
     SidebarItems: typeof import('./src/components/Sidebar/SidebarItems.vue')['default']
-    Skeleton: typeof import('primevue/skeleton')['default']
-    Slider: typeof import('primevue/slider')['default']
-    Toast: typeof import('primevue/toast')['default']
-    TopBar: typeof import('./src/components/TopBar.vue')['default']
-  }
-  export interface ComponentCustomProperties {
-    Ripple: typeof import('primevue/ripple')['default']
   }
 }
